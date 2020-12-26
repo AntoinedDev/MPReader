128e015c576b766c630d6e1acb0aa6e32c8a3853
arch <arch@pop-os.localdomain>
2020-12-26 20:35:29 +0100

    Begining webscrapping

--- MPReader.py
+++ MPReader.py
@@ -2,6 +2,8 @@ import os
 import sys
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
+import requests 
+
 
 browser = webdriver.Chrome('/home/arch/myDev/chromedriver')
 browser.get('https://www.mediapart.fr/login')
@@ -13,15 +15,11 @@ def authenticate():
     python_button.send_keys('Jerry1994')
     # python_button = browser.find_element_by_xpath("//*[@id='logFormEl'][1]")
     python_button.submit()
-
-# def something_interresting():
-
+    url = browser.current_url
 
 
 
 if __name__ == '__main__':
     authenticate()
-
-
 
 
