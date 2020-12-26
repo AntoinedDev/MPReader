9116766ff09ecc83be84f0ca0796782a6511c453
arch <arch@pop-os.localdomain>
2020-12-26 20:18:09 +0100

    Adding connection options + requierments.txt

---
+++ MPReader.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+
+browser = webdriver.Chrome('/home/arch/myDev/chromedriver')
+browser.get('https://www.mediapart.fr/login')
+
+def authenticate():
+    python_button = browser.find_element_by_xpath("//*[@id='edit-name-page']")
+    python_button.send_keys('darboisantoine@gmail.com')
+    python_button = browser.find_element_by_xpath("//*[@id='edit-pass-page']")
+    python_button.send_keys('Jerry1994')
+    # python_button = browser.find_element_by_xpath("//*[@id='logFormEl'][1]")
+    python_button.submit()
+
+# def something_interresting():
+
+
+
+
+if __name__ == '__main__':
+    authenticate()
+
+
+
+
